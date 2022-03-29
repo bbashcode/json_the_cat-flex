5116ef071bda18ad6d30788f76ec464ce22efa20
bbashcode <binbash.code@gmail.com>
2022-03-29 06:27:05 +0000

    fix cat as a service program to fetch Siberian cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,24 @@
-const request = require("request");
-const url = "https://api.thecatapi.com/v1/images/search?q=Siberian";
+const request = require('request');
 
+let userInput = process.argv.slice(2);
+let url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
 
-request(url, (error, response, body)=>{
+request((url), (error, response, body) => {
 
-  if(error){
-    console.log("error: ", error);
-  }
+    if (error) {
+        console.log(error);
+        console.log({
+            error
+        });
+    }
+    console.log(response);
+    console.log(body);
+    console.log(`body: `, body);
 
-  console.log("response: ", response, "status code: ", response.statusCode);
-  // console.log("data: ", data[0], "type of data: ", typeof data);
-  console.log("body: ", body);
-  console.log("type of body: ", typeof body);
-  const data = JSON.parse(body);
-  console.log("data: ", data);
-  console.log("type of data: ",typeof data);
-  console.log("print the first entry in data: ", data[0]);
+    console.log(`type of body`, typeof body);
+
+    console.log('--------------------------------------');
+    const data = JSON.parse(body);
+    console.log(`data`, data[0].description);
+    console.log(`type of data`, typeof data);
 });
