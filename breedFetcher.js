00da8c59b9ad5cc22f56f2a1de6ce3d71f0f4333
bbashcode <binbash.code@gmail.com>
2022-03-29 06:36:10 +0000

    fix eslint issues

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,22 +7,22 @@ let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInput;
 
 request((url), (error, response, body) => {
 
-	// Handle request errors and print the error details to the screen.  
-	if (error) {
-		console.log('Here is the error: ', error);
-	}
+  // Handle request errors and print the error details to the screen.
+  if (error) {
+    console.log('Here is the error: ', error);
+  }
 
 
-	// use JSON.parse to convert the JSON string into an actual object.
-	const data = JSON.parse(body);
+  // use JSON.parse to convert the JSON string into an actual object.
+  const data = JSON.parse(body);
 
-	// Edge Case: Breed Not Found
-	if (data.length === 0 || data[0] === 'undefined') {
-		console.log(`Sorry, the requested breed: '${userInput}' cannot be found!`);
-	} else { // access the first entry in the data array and print out the description for the user.
-		console.log(`Breed Description: `, data[0].description);
-		console.log(`Breed Details: `, data[0]);
+  // Edge Case: Breed Not Found
+  if (data.length === 0 || data[0] === 'undefined') {
+    console.log(`Sorry, the requested breed: '${userInput}' cannot be found!`);
+  } else { // access the first entry in the data array and print out the description for the user.
+    console.log(`Breed Description: `, data[0].description);
+    console.log(`Breed Details: `, data[0]);
 
-	}
+  }
 
 });
